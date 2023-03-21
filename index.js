d6468c341822abb68e3eab7bfe3ba22c10715091
Muhammed Nibras <mohdnibras1998@gmail.com>
2023-03-21 22:39:13 +0530

    authentication added backend

--- index.js
+++ index.js
@@ -4,18 +4,38 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors')
 const mime = require('mime');
+const CLIENT_ID = "395472076605-2iba3s4npuik7tdgshi84e3s0si3l247.apps.googleusercontent.com"
+const axios = require('axios');
+
+async function verifyToken(token) {
+    try {
+        const response = await axios.get(`https://oauth2.googleapis.com/tokeninfo?access_token=${token}`);
+        const { data } = response;
+        if (data.aud !== CLIENT_ID) {
+            throw new Error('Invalid client ID');
+        }
+        return data;
+    } catch (error) {
+        return null
+
+    }
+}
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 const port = process.env.PORT || 5000;
 
+
+
+
+
 const upload = (userId) => {
     const userDirectory = `./uploads/${userId}`;
-    if (!fs.existsSync(userDirectory)){
+    if (!fs.existsSync(userDirectory)) {
         fs.mkdirSync(userDirectory);
     }
-    
+
     const storage = multer.diskStorage({
         destination: userDirectory,
         filename: function (req, file, cb) {
@@ -34,10 +54,21 @@ const upload = (userId) => {
 }
 
 
-
-
-app.post('/api/upload/:userId', (req, res) => {
+app.post('/api/upload/:userId', async (req, res) => {
     const userId = req.params.userId;
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+        console.log("no", authorizationHeader)
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const accessToken = authorizationHeader.split(' ')[1];
+    const verifier = await verifyToken(accessToken);
+    console.log(verifier)
+    if (!verifier) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
     const uploader = upload(userId);
     uploader(req, res, (err) => {
         if (err) {
@@ -49,14 +80,28 @@ app.post('/api/upload/:userId', (req, res) => {
 });
 
 
-
-
-app.get('/api/files/:userId', (req, res) => {
+app.get('/api/files/:userId', async (req, res) => {
     const userId = req.params.userId;
+    const authorizationHeader = req.headers.authorization;
+    console.log(authorizationHeader)
+    if (!authorizationHeader) {
+
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    const verifier = await verifyToken(accessToken);
+    console.log(verifier)
+    if (!verifier) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+
     const userDirectory = `./uploads/${userId}`;
     fs.readdir(userDirectory, (err, files) => {
         if (err) {
-            res.status(500).json({ message: err.message, cause:"No files to show" });
+            res.status(500).json({ message: err.message, cause: "No files to show" });
         } else {
             const fileNames = files.map((file) => ({ name: file, type: mime.lookup(file) }));
             res.status(200).json({ files: fileNames });
@@ -65,15 +110,44 @@ app.get('/api/files/:userId', (req, res) => {
 });
 
 
-
-
-app.get('/api/files/:userId/:filename', (req, res) => {
+app.get('/api/files/:userId/:filename', async (req, res) => {
     const userId = req.params.userId;
     const filename = req.params.filename;
     const filepath = path.join(__dirname, 'uploads', userId, filename);
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+        console.log("no", authorizationHeader)
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    const verifier = await verifyToken(accessToken);
+    if (!verifier) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
     res.sendFile(filepath);
 });
 
+app.get('/api/files/display/:userId/:filename/:accessToken', async (req, res) => {
+    const userId = req.params.userId;
+    const accessToken = req.params.accessToken
+    const filename = req.params.filename;
+    const filepath = path.join(__dirname, 'uploads', userId, filename);
 
+    if (!accessToken) {
+
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+
+    const verifier = await verifyToken(accessToken);
+
+    if (!verifier) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    res.sendFile(filepath);
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
